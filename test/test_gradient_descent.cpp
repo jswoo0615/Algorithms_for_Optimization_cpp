9cff0397163c54e6dd49fedeb9fb0ed7eb3138ae
jswoo0615 <128343128+jswoo0615@users.noreply.github.com>
2026-03-18 12:05:34 +0000

    style: auto-format code with clang-format

--- test/test_gradient_descent.cpp
+++ test/test_gradient_descent.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+
 #include "Optimization/GradientDescent.hpp"
 
 using namespace Optimization;
@@ -6,8 +7,8 @@ using namespace Optimization;
 TEST(OptimizerTest, GradientDescentRosenbrock) {
     auto rosenbrock = [](const auto& v) {
         auto term1 = 1.0 - v[0];
-        auto term2 = v[1] - v[0]*v[0];
-        return term1*term1 + 100.0 * term2*term2;
+        auto term2 = v[1] - v[0] * v[0];
+        return term1 * term1 + 100.0 * term2 * term2;
     };
 
     std::array<double, 2> start = {-1.2, 1.0};
