e1097d4f9e55e7f9fa3a1f992b7c11e948cac333
jswoo0615 <jswoo0615@users.noreply.github.com>
2026-03-21 07:05:31 +0000

    deploy: ff0870f3168350ecdeab796d129c619b1ea49a36

--- search/all_0.js
+++ search/all_0.js
@@ -2,6 +2,8 @@ var searchData=
 [
   ['adagrad_0',['AdaGrad',['../classOptimization_1_1AdaGrad.html',1,'Optimization']]],
   ['adagradtest_1',['AdaGradTest',['../classOptimization_1_1Test_1_1AdaGradTest.html',1,'Optimization::Test']]],
-  ['autodiff_2',['AutoDiff',['../classOptimization_1_1AutoDiff.html',1,'Optimization']]],
-  ['autodifftest_3',['AutoDiffTest',['../classAutoDiffTest.html',1,'']]]
+  ['adam_2',['Adam',['../classOptimization_1_1Adam.html',1,'Optimization']]],
+  ['adamtest_3',['AdamTest',['../classOptimization_1_1Test_1_1AdamTest.html',1,'Optimization::Test']]],
+  ['autodiff_4',['AutoDiff',['../classOptimization_1_1AutoDiff.html',1,'Optimization']]],
+  ['autodifftest_5',['AutoDiffTest',['../classAutoDiffTest.html',1,'']]]
 ];
