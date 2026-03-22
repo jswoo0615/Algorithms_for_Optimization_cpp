01f9ed2c47d40b82f6dc24f0979a2f463f6c0306
jswoo0615 <jswoo0615@users.noreply.github.com>
2026-03-22 07:47:25 +0000

    deploy: 74e5774f1bd9ae79fb7b5d84c63cebda5b97d92b

--- search/classes_9.js
+++ search/classes_9.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['nestrovmomentum_0',['NestrovMomentum',['../classOptimization_1_1NestrovMomentum.html',1,'Optimization']]],
-  ['nestrovmomentumtest_1',['NestrovMomentumTest',['../classOptimization_1_1Test_1_1NestrovMomentumTest.html',1,'Optimization::Test']]],
-  ['newtonmethod_2',['NewtonMethod',['../classOptimization_1_1NewtonMethod.html',1,'Optimization']]],
-  ['nondifferentiable2d_3',['NonDifferentiable2D',['../structOptimization_1_1Tests_1_1NonDifferentiable2D.html',1,'Optimization::Tests']]]
+  ['neldermead_0',['NelderMead',['../classOptimization_1_1NelderMead.html',1,'Optimization']]],
+  ['nestrovmomentum_1',['NestrovMomentum',['../classOptimization_1_1NestrovMomentum.html',1,'Optimization']]],
+  ['nestrovmomentumtest_2',['NestrovMomentumTest',['../classOptimization_1_1Test_1_1NestrovMomentumTest.html',1,'Optimization::Test']]],
+  ['newtonmethod_3',['NewtonMethod',['../classOptimization_1_1NewtonMethod.html',1,'Optimization']]],
+  ['nondifferentiable2d_4',['NonDifferentiable2D',['../structOptimization_1_1Tests_1_1NonDifferentiable2D.html',1,'Optimization::Tests']]]
 ];
