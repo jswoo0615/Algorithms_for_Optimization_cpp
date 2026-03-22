a2de6934fcb4845750c7bc806fb5371da45ae9a6
jswoo0615 <jswoo0615@users.noreply.github.com>
2026-03-22 02:42:50 +0000

    deploy: e971ed1d8757f6c12abb69fe11e19401366b296a

--- search/all_2.js
+++ search/all_2.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['conjugategradient_0',['ConjugateGradient',['../classOptimization_1_1ConjugateGradient.html',1,'Optimization']]],
-  ['conjugategradienttest_1',['ConjugateGradientTest',['../classConjugateGradientTest.html',1,'']]]
+  ['conjugategradienttest_1',['ConjugateGradientTest',['../classConjugateGradientTest.html',1,'']]],
+  ['convexquadratic2d_2',['ConvexQuadratic2D',['../structOptimization_1_1Tests_1_1ConvexQuadratic2D.html',1,'Optimization::Tests']]],
+  ['cycliccoordinatesearch_3',['CyclicCoordinateSearch',['../classOptimization_1_1CyclicCoordinateSearch.html',1,'Optimization']]]
 ];
--- search/classes_4.js
+++ search/classes_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['gradientdescent_0',['GradientDescent',['../classOptimization_1_1GradientDescent.html',1,'Optimization']]]
+  ['goldensectionstrategy_0',['GoldenSectionStrategy',['../structOptimization_1_1GoldenSectionStrategy.html',1,'Optimization']]],
+  ['gradientdescent_1',['GradientDescent',['../classOptimization_1_1GradientDescent.html',1,'Optimization']]]
 ];
