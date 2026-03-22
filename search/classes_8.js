a2492b9bf5c22e0b8e92176f6d8b113b19890f6d
jswoo0615 <jswoo0615@users.noreply.github.com>
2026-03-22 22:29:09 +0000

    deploy: e3452dbca223fb6f6e0ab8c3bc7e52a4b546b8c2

--- search/classes_8.js
+++ search/classes_8.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['momentum_0',['Momentum',['../classOptimization_1_1Momentum.html',1,'Optimization']]],
-  ['momentumoptimizertest_1',['MomentumOptimizerTest',['../classOptimization_1_1Test_1_1MomentumOptimizerTest.html',1,'Optimization::Test']]]
+  ['meshadaptivedirectsearch_0',['MeshAdaptiveDirectSearch',['../classOptimization_1_1MeshAdaptiveDirectSearch.html',1,'Optimization']]],
+  ['momentum_1',['Momentum',['../classOptimization_1_1Momentum.html',1,'Optimization']]],
+  ['momentumoptimizertest_2',['MomentumOptimizerTest',['../classOptimization_1_1Test_1_1MomentumOptimizerTest.html',1,'Optimization::Test']]]
 ];
