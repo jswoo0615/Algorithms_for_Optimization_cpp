31cc3819ba9f98194a1becffa40cf778e9c5c97b
jswoo0615 <jswoo0615@users.noreply.github.com>
2026-03-22 03:01:54 +0000

    deploy: 316e795a74d3e9e034b8b7f300159a799715218b

--- search/classes_a.js
+++ search/classes_a.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['pt_0',['Pt',['../structOptimization_1_1Pt.html',1,'Optimization']]]
+  ['powellmethod_0',['PowellMethod',['../classOptimization_1_1PowellMethod.html',1,'Optimization']]],
+  ['pt_1',['Pt',['../structOptimization_1_1Pt.html',1,'Optimization']]]
 ];
