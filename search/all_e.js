83302b423af48a521ee95be940618077170b54fb
jswoo0615 <jswoo0615@users.noreply.github.com>
2026-03-27 03:26:54 +0000

    deploy: 18ab67ef415e8a3195508f34756aa27c57e1cd9b

--- search/all_e.js
+++ search/all_e.js
@@ -1,7 +1,8 @@
 var searchData=
 [
   ['secantmethod_0',['SecantMethod',['../classOptimization_1_1SecantMethod.html',1,'Optimization']]],
-  ['spherefunction_1',['SphereFunction',['../structOptimization_1_1Tests_1_1SphereFunction.html',1,'Optimization::Tests']]],
-  ['strongbacktrackinglinesearch_2',['StrongBacktrackingLineSearch',['../classOptimization_1_1StrongBacktrackingLineSearch.html',1,'Optimization']]],
-  ['strongwolfetest_3',['StrongWolfeTest',['../classStrongWolfeTest.html',1,'']]]
+  ['simulatedannealing_1',['SimulatedAnnealing',['../classOptimization_1_1SimulatedAnnealing.html',1,'Optimization']]],
+  ['spherefunction_2',['SphereFunction',['../structOptimization_1_1Tests_1_1SphereFunction.html',1,'Optimization::Tests']]],
+  ['strongbacktrackinglinesearch_3',['StrongBacktrackingLineSearch',['../classOptimization_1_1StrongBacktrackingLineSearch.html',1,'Optimization']]],
+  ['strongwolfetest_4',['StrongWolfeTest',['../classStrongWolfeTest.html',1,'']]]
 ];
