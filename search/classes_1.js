aba80f20e43c9429a0365164e0ad5917e0cc755a
jswoo0615 <jswoo0615@users.noreply.github.com>
2026-03-28 00:52:31 +0000

    deploy: 8cb3564652d845addab6f274f8c57ae9c62e6101

--- search/classes_1.js
+++ search/classes_1.js
@@ -2,7 +2,8 @@ var searchData=
 [
   ['backtrackinglinesearch_0',['BacktrackingLineSearch',['../classOptimization_1_1BacktrackingLineSearch.html',1,'Optimization']]],
   ['backtrackingtest_1',['BacktrackingTest',['../classBacktrackingTest.html',1,'']]],
-  ['bfgs_2',['BFGS',['../classOptimization_1_1BFGS.html',1,'Optimization']]],
-  ['bracketingtest_3',['BracketingTest',['../classBracketingTest.html',1,'']]],
-  ['braninfunction_4',['BraninFunction',['../structOptimization_1_1Tests_1_1BraninFunction.html',1,'Optimization::Tests']]]
+  ['bealefunction_2',['BealeFunction',['../structOptimization_1_1Tests_1_1BealeFunction.html',1,'Optimization::Tests']]],
+  ['bfgs_3',['BFGS',['../classOptimization_1_1BFGS.html',1,'Optimization']]],
+  ['bracketingtest_4',['BracketingTest',['../classBracketingTest.html',1,'']]],
+  ['braninfunction_5',['BraninFunction',['../structOptimization_1_1Tests_1_1BraninFunction.html',1,'Optimization::Tests']]]
 ];
