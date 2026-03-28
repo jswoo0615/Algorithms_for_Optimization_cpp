a27eafb1b6a10386d847ee82e6d9422944b304c2
jswoo0615 <jswoo0615@users.noreply.github.com>
2026-03-28 01:29:08 +0000

    deploy: f3cb398f2f4108396a11d5eec819a650e49db369

--- search/classes_0.js
+++ search/classes_0.js
@@ -1,10 +1,11 @@
 var searchData=
 [
-  ['adadelta_0',['AdaDelta',['../classOptimization_1_1AdaDelta.html',1,'Optimization']]],
-  ['adagrad_1',['AdaGrad',['../classOptimization_1_1AdaGrad.html',1,'Optimization']]],
-  ['adagradtest_2',['AdaGradTest',['../classOptimization_1_1Test_1_1AdaGradTest.html',1,'Optimization::Test']]],
-  ['adam_3',['Adam',['../classOptimization_1_1Adam.html',1,'Optimization']]],
-  ['adamtest_4',['AdamTest',['../classOptimization_1_1Test_1_1AdamTest.html',1,'Optimization::Test']]],
-  ['autodiff_5',['AutoDiff',['../classOptimization_1_1AutoDiff.html',1,'Optimization']]],
-  ['autodifftest_6',['AutoDiffTest',['../classAutoDiffTest.html',1,'']]]
+  ['ackley2d_0',['Ackley2D',['../structOptimization_1_1Tests_1_1Ackley2D.html',1,'Optimization::Tests']]],
+  ['adadelta_1',['AdaDelta',['../classOptimization_1_1AdaDelta.html',1,'Optimization']]],
+  ['adagrad_2',['AdaGrad',['../classOptimization_1_1AdaGrad.html',1,'Optimization']]],
+  ['adagradtest_3',['AdaGradTest',['../classOptimization_1_1Test_1_1AdaGradTest.html',1,'Optimization::Test']]],
+  ['adam_4',['Adam',['../classOptimization_1_1Adam.html',1,'Optimization']]],
+  ['adamtest_5',['AdamTest',['../classOptimization_1_1Test_1_1AdamTest.html',1,'Optimization::Test']]],
+  ['autodiff_6',['AutoDiff',['../classOptimization_1_1AutoDiff.html',1,'Optimization']]],
+  ['autodifftest_7',['AutoDiffTest',['../classAutoDiffTest.html',1,'']]]
 ];
--- search/classes_2.js
+++ search/classes_2.js
@@ -3,5 +3,6 @@ var searchData=
   ['conjugategradient_0',['ConjugateGradient',['../classOptimization_1_1ConjugateGradient.html',1,'Optimization']]],
   ['conjugategradienttest_1',['ConjugateGradientTest',['../classConjugateGradientTest.html',1,'']]],
   ['convexquadratic2d_2',['ConvexQuadratic2D',['../structOptimization_1_1Tests_1_1ConvexQuadratic2D.html',1,'Optimization::Tests']]],
-  ['cycliccoordinatesearch_3',['CyclicCoordinateSearch',['../classOptimization_1_1CyclicCoordinateSearch.html',1,'Optimization']]]
+  ['crossentropy_3',['CrossEntropy',['../classOptimization_1_1CrossEntropy.html',1,'Optimization']]],
+  ['cycliccoordinatesearch_4',['CyclicCoordinateSearch',['../classOptimization_1_1CyclicCoordinateSearch.html',1,'Optimization']]]
 ];
