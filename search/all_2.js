349a8c8e0efa0ab43f9ab80f3b0bccbd976724a1
jswoo0615 <jswoo0615@users.noreply.github.com>
2026-03-31 11:09:43 +0000

    deploy: 1410049f269aedf6132f7c1b7db530cf239defe2

--- search/all_2.js
+++ search/all_2.js
@@ -1,8 +1,9 @@
 var searchData=
 [
-  ['conjugategradient_0',['ConjugateGradient',['../classOptimization_1_1ConjugateGradient.html',1,'Optimization']]],
-  ['conjugategradienttest_1',['ConjugateGradientTest',['../classConjugateGradientTest.html',1,'']]],
-  ['convexquadratic2d_2',['ConvexQuadratic2D',['../structOptimization_1_1Tests_1_1ConvexQuadratic2D.html',1,'Optimization::Tests']]],
-  ['crossentropy_3',['CrossEntropy',['../classOptimization_1_1CrossEntropy.html',1,'Optimization']]],
-  ['cycliccoordinatesearch_4',['CyclicCoordinateSearch',['../classOptimization_1_1CyclicCoordinateSearch.html',1,'Optimization']]]
+  ['cma_5fes_0',['CMA_ES',['../classOptimization_1_1CMA__ES.html',1,'Optimization']]],
+  ['conjugategradient_1',['ConjugateGradient',['../classOptimization_1_1ConjugateGradient.html',1,'Optimization']]],
+  ['conjugategradienttest_2',['ConjugateGradientTest',['../classConjugateGradientTest.html',1,'']]],
+  ['convexquadratic2d_3',['ConvexQuadratic2D',['../structOptimization_1_1Tests_1_1ConvexQuadratic2D.html',1,'Optimization::Tests']]],
+  ['crossentropy_4',['CrossEntropy',['../classOptimization_1_1CrossEntropy.html',1,'Optimization']]],
+  ['cycliccoordinatesearch_5',['CyclicCoordinateSearch',['../classOptimization_1_1CyclicCoordinateSearch.html',1,'Optimization']]]
 ];
--- search/all_d.js
+++ search/all_d.js
@@ -3,6 +3,7 @@ var searchData=
   ['range_0',['Range',['../structOptimization_1_1Range.html',1,'Optimization']]],
   ['rmsprop_1',['RMSProp',['../classOptimization_1_1RMSProp.html',1,'Optimization']]],
   ['rmsproptest_2',['RMSPropTest',['../classOptimization_1_1Test_1_1RMSPropTest.html',1,'Optimization::Test']]],
-  ['rosenbrockfunc_3',['RosenbrockFunc',['../structOptimization_1_1Test_1_1RosenbrockFunc.html',1,'Optimization::Test']]],
-  ['rosenbrockfunction_4',['RosenbrockFunction',['../structOptimization_1_1Tests_1_1RosenbrockFunction.html',1,'Optimization::Tests']]]
+  ['rosenbrock2d_3',['Rosenbrock2D',['../structOptimization_1_1Tests_1_1Rosenbrock2D.html',1,'Optimization::Tests']]],
+  ['rosenbrockfunc_4',['RosenbrockFunc',['../structOptimization_1_1Test_1_1RosenbrockFunc.html',1,'Optimization::Test']]],
+  ['rosenbrockfunction_5',['RosenbrockFunction',['../structOptimization_1_1Tests_1_1RosenbrockFunction.html',1,'Optimization::Tests']]]
 ];
