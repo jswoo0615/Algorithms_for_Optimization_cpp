651175561658f6c884b23411dc2994df747857b9
jswoo0615 <jswoo0615@users.noreply.github.com>
2026-04-11 12:19:11 +0000

    deploy: bc2f29cfbfca57afda3aaf36fa22a717099f93cb

--- search/all_10.js
+++ search/all_10.js
@@ -1,12 +1,13 @@
 var searchData=
 [
   ['range_0',['Range',['../structOptimization_1_1Range.html',1,'Optimization']]],
-  ['rmsprop_1',['RMSProp',['../classOptimization_1_1RMSProp.html',1,'Optimization']]],
-  ['rmsproptest_2',['RMSPropTest',['../classOptimization_1_1Test_1_1RMSPropTest.html',1,'Optimization::Test']]],
-  ['rosenbrock2d_3',['Rosenbrock2D',['../structOptimization_1_1Tests_1_1Rosenbrock2D.html',1,'Optimization::Tests']]],
-  ['rosenbrockfunc_4',['RosenbrockFunc',['../structOptimization_1_1Test_1_1RosenbrockFunc.html',1,'Optimization::Test']]],
-  ['rosenbrockfunction_5',['RosenbrockFunction',['../structOptimization_1_1Tests_1_1RosenbrockFunction.html',1,'Optimization::Tests']]],
-  ['rtinmpccontroller_6',['RTINMPCController',['../classOptimization_1_1RTINMPCController.html',1,'Optimization']]],
-  ['rtiobstacle_7',['RTIObstacle',['../structOptimization_1_1RTINMPCController_1_1RTIObstacle.html',1,'Optimization::RTINMPCController']]],
-  ['rtisolver_8',['RTISolver',['../classOptimization_1_1RTISolver.html',1,'Optimization']]]
+  ['residualfunc_1',['ResidualFunc',['../structOptimization_1_1RTINMPCController_1_1ResidualFunc.html',1,'Optimization::RTINMPCController']]],
+  ['rmsprop_2',['RMSProp',['../classOptimization_1_1RMSProp.html',1,'Optimization']]],
+  ['rmsproptest_3',['RMSPropTest',['../classOptimization_1_1Test_1_1RMSPropTest.html',1,'Optimization::Test']]],
+  ['rosenbrock2d_4',['Rosenbrock2D',['../structOptimization_1_1Tests_1_1Rosenbrock2D.html',1,'Optimization::Tests']]],
+  ['rosenbrockfunc_5',['RosenbrockFunc',['../structOptimization_1_1Test_1_1RosenbrockFunc.html',1,'Optimization::Test']]],
+  ['rosenbrockfunction_6',['RosenbrockFunction',['../structOptimization_1_1Tests_1_1RosenbrockFunction.html',1,'Optimization::Tests']]],
+  ['rtinmpccontroller_7',['RTINMPCController',['../classOptimization_1_1RTINMPCController.html',1,'Optimization']]],
+  ['rtiobstacle_8',['RTIObstacle',['../structOptimization_1_1RTINMPCController_1_1RTIObstacle.html',1,'Optimization::RTINMPCController']]],
+  ['rtisolver_9',['RTISolver',['../classOptimization_1_1RTISolver.html',1,'Optimization']]]
 ];
