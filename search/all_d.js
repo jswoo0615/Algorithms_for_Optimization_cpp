0a0ef7451ee097ab357293c04eccc40ab2d42475
jswoo0615 <jswoo0615@users.noreply.github.com>
2026-04-12 01:18:32 +0000

    deploy: 47a198855b492881ad88b36fda9a7cdb1d157c84

--- search/all_d.js
+++ search/all_d.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['naturalevolutionstrategies_0',['NaturalEvolutionStrategies',['../classOptimization_1_1NaturalEvolutionStrategies.html',1,'Optimization']]],
-  ['near_5fzero_1',['near_zero',['../structMathTraits.html#ae4bdc59135c8d5d2cb6093d6e7208758',1,'MathTraits']]],
+  ['near_5fzero_1',['near_zero',['../structMathTraits.html#ae4bdc59135c8d5d2cb6093d6e7208758',1,'MathTraits::near_zero()'],['../structMathTraits_3_01Optimization_1_1Dual_3_01T_01_4_01_4.html#a5ce19b2b6c719ee9b20d4349544d65e7',1,'MathTraits&lt; Optimization::Dual&lt; T &gt; &gt;::near_zero()']]],
   ['neldermead_2',['NelderMead',['../classOptimization_1_1NelderMead.html',1,'Optimization']]],
   ['nestrovmomentum_3',['NestrovMomentum',['../classOptimization_1_1NestrovMomentum.html',1,'Optimization']]],
   ['nestrovmomentumtest_4',['NestrovMomentumTest',['../classOptimization_1_1Test_1_1NestrovMomentumTest.html',1,'Optimization::Test']]],
--- search/functions_7.js
+++ search/functions_7.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['near_5fzero_0',['near_zero',['../structMathTraits.html#ae4bdc59135c8d5d2cb6093d6e7208758',1,'MathTraits']]]
+  ['near_5fzero_0',['near_zero',['../structMathTraits.html#ae4bdc59135c8d5d2cb6093d6e7208758',1,'MathTraits::near_zero()'],['../structMathTraits_3_01Optimization_1_1Dual_3_01T_01_4_01_4.html#a5ce19b2b6c719ee9b20d4349544d65e7',1,'MathTraits&lt; Optimization::Dual&lt; T &gt; &gt;::near_zero()']]]
 ];
