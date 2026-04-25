b74b06788e5a1b76bdfab8cf724fed399769d643
jswoo0615 <jswoo0615@users.noreply.github.com>
2026-04-25 10:40:04 +0000

    deploy: 8a5b1e01a85a5b428ae002843beeb21c402c5be6

--- search/functions_6.js
+++ search/functions_6.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['golden_5fsection_5fsearch_0',['golden_section_search',['../classOptimization_1_1LineSearch.html#ae0f16aea51b36b664bfe865a57267e8a',1,'Optimization::LineSearch']]],
-  ['gradient_1',['gradient',['../classOptimization_1_1AutoDiff.html#a58e4c087ad635622012694e6851455b1',1,'Optimization::AutoDiff']]]
+  ['generate_5fslalom_5freference_0',['generate_slalom_reference',['../classOptimization_1_1PathPlanner.html#a0c66fa4bdc1e5657bfa939dd48c47db0',1,'Optimization::PathPlanner']]],
+  ['golden_5fsection_5fsearch_1',['golden_section_search',['../classOptimization_1_1LineSearch.html#ae0f16aea51b36b664bfe865a57267e8a',1,'Optimization::LineSearch']]],
+  ['gradient_2',['gradient',['../classOptimization_1_1AutoDiff.html#a58e4c087ad635622012694e6851455b1',1,'Optimization::AutoDiff']]]
 ];
