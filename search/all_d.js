a16cc78ca6252ed4d2f72734aced9236933c6d0c
jswoo0615 <jswoo0615@users.noreply.github.com>
2026-04-26 07:42:33 +0000

    deploy: 96132d143ca2820d3458881e78f1fe206222de5d

--- search/all_d.js
+++ search/all_d.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['naturalevolutionstrategies_0',['NaturalEvolutionStrategies',['../classOptimization_1_1NaturalEvolutionStrategies.html',1,'Optimization']]],
-  ['near_5fzero_1',['near_zero',['../structMathTraits.html#ae4bdc59135c8d5d2cb6093d6e7208758',1,'MathTraits::near_zero()'],['../structMathTraits_3_01Optimization_1_1Dual_3_01T_01_4_01_4.html#a5ce19b2b6c719ee9b20d4349544d65e7',1,'MathTraits&lt; Optimization::Dual&lt; T &gt; &gt;::near_zero()']]],
+  ['near_5fzero_1',['near_zero',['../structOptimization_1_1MathTraits.html#a855877cf52707ffd184411d39ff0303f',1,'Optimization::MathTraits::near_zero()'],['../structOptimization_1_1MathTraits_3_01Optimization_1_1Dual_3_01T_01_4_01_4.html#ae0229121a23f6a60abc743138d10e961',1,'Optimization::MathTraits&lt; Optimization::Dual&lt; T &gt; &gt;::near_zero()'],['../structMathTraits.html#ae4bdc59135c8d5d2cb6093d6e7208758',1,'MathTraits::near_zero()'],['../structMathTraits_3_01Optimization_1_1Dual_3_01T_01_4_01_4.html#a5ce19b2b6c719ee9b20d4349544d65e7',1,'MathTraits&lt; Optimization::Dual&lt; T &gt; &gt;::near_zero()']]],
   ['neldermead_2',['NelderMead',['../classOptimization_1_1NelderMead.html',1,'Optimization']]],
   ['nestrovmomentum_3',['NestrovMomentum',['../classOptimization_1_1NestrovMomentum.html',1,'Optimization']]],
   ['nestrovmomentumtest_4',['NestrovMomentumTest',['../classOptimization_1_1Test_1_1NestrovMomentumTest.html',1,'Optimization::Test']]],
--- search/functions_a.js
+++ search/functions_a.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['near_5fzero_0',['near_zero',['../structMathTraits.html#ae4bdc59135c8d5d2cb6093d6e7208758',1,'MathTraits::near_zero()'],['../structMathTraits_3_01Optimization_1_1Dual_3_01T_01_4_01_4.html#a5ce19b2b6c719ee9b20d4349544d65e7',1,'MathTraits&lt; Optimization::Dual&lt; T &gt; &gt;::near_zero()']]],
+  ['near_5fzero_0',['near_zero',['../structOptimization_1_1MathTraits.html#a855877cf52707ffd184411d39ff0303f',1,'Optimization::MathTraits::near_zero()'],['../structOptimization_1_1MathTraits_3_01Optimization_1_1Dual_3_01T_01_4_01_4.html#ae0229121a23f6a60abc743138d10e961',1,'Optimization::MathTraits&lt; Optimization::Dual&lt; T &gt; &gt;::near_zero()'],['../structMathTraits.html#ae4bdc59135c8d5d2cb6093d6e7208758',1,'MathTraits::near_zero()'],['../structMathTraits_3_01Optimization_1_1Dual_3_01T_01_4_01_4.html#a5ce19b2b6c719ee9b20d4349544d65e7',1,'MathTraits&lt; Optimization::Dual&lt; T &gt; &gt;::near_zero()']]],
   ['nmpcstoragedata_1',['NMPCStorageData',['../structOptimization_1_1NMPCStorageData.html#aea94e3ad87497b8459f40811eaf2ff7e',1,'Optimization::NMPCStorageData']]]
 ];
