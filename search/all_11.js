208953753c0768e57ec8148ac4b5bb1a23cf9e36
jswoo0615 <jswoo0615@users.noreply.github.com>
2026-05-03 04:45:53 +0000

    deploy: e66e63f76a0767aca45be3a28bd6ffe7c3c0c63b

--- search/all_11.js
+++ search/all_11.js
@@ -3,13 +3,14 @@ var searchData=
   ['r_0',['r',['../structOptimization_1_1NMPCStorageData.html#ae025b940e728aad84a47922ba4ae4db3',1,'Optimization::NMPCStorageData::r'],['../structOptimization_1_1NMPCStorageData.html#a365dc32b5b5e8555bb6a3b407dbe9295',1,'Optimization::NMPCStorageData::R']]],
   ['range_1',['Range',['../structOptimization_1_1Range.html',1,'Optimization']]],
   ['residualfunc_2',['ResidualFunc',['../structOptimization_1_1RTINMPCController_1_1ResidualFunc.html',1,'Optimization::RTINMPCController']]],
-  ['rmsprop_3',['RMSProp',['../classOptimization_1_1RMSProp.html',1,'Optimization']]],
-  ['rmsproptest_4',['RMSPropTest',['../classOptimization_1_1Test_1_1RMSPropTest.html',1,'Optimization::Test']]],
-  ['rosenbrock2d_5',['Rosenbrock2D',['../structOptimization_1_1Tests_1_1Rosenbrock2D.html',1,'Optimization::Tests']]],
-  ['rosenbrockfunc_6',['RosenbrockFunc',['../structOptimization_1_1Test_1_1RosenbrockFunc.html',1,'Optimization::Test']]],
-  ['rosenbrockfunction_7',['RosenbrockFunction',['../structOptimization_1_1Tests_1_1RosenbrockFunction.html',1,'Optimization::Tests']]],
-  ['rosenbrockresiduals_8',['RosenbrockResiduals',['../structRosenbrockResiduals.html',1,'']]],
-  ['rtinmpccontroller_9',['RTINMPCController',['../classOptimization_1_1RTINMPCController.html',1,'Optimization']]],
-  ['rtiobstacle_10',['RTIObstacle',['../structOptimization_1_1RTINMPCController_1_1RTIObstacle.html',1,'Optimization::RTINMPCController']]],
-  ['rtisolver_11',['RTISolver',['../classOptimization_1_1RTISolver.html',1,'Optimization']]]
+  ['riccatisolver_3',['RiccatiSolver',['../structOptimization_1_1solver_1_1RiccatiSolver.html',1,'Optimization::solver']]],
+  ['rmsprop_4',['RMSProp',['../classOptimization_1_1RMSProp.html',1,'Optimization']]],
+  ['rmsproptest_5',['RMSPropTest',['../classOptimization_1_1Test_1_1RMSPropTest.html',1,'Optimization::Test']]],
+  ['rosenbrock2d_6',['Rosenbrock2D',['../structOptimization_1_1Tests_1_1Rosenbrock2D.html',1,'Optimization::Tests']]],
+  ['rosenbrockfunc_7',['RosenbrockFunc',['../structOptimization_1_1Test_1_1RosenbrockFunc.html',1,'Optimization::Test']]],
+  ['rosenbrockfunction_8',['RosenbrockFunction',['../structOptimization_1_1Tests_1_1RosenbrockFunction.html',1,'Optimization::Tests']]],
+  ['rosenbrockresiduals_9',['RosenbrockResiduals',['../structRosenbrockResiduals.html',1,'']]],
+  ['rtinmpccontroller_10',['RTINMPCController',['../classOptimization_1_1RTINMPCController.html',1,'Optimization']]],
+  ['rtiobstacle_11',['RTIObstacle',['../structOptimization_1_1RTINMPCController_1_1RTIObstacle.html',1,'Optimization::RTINMPCController']]],
+  ['rtisolver_12',['RTISolver',['../classOptimization_1_1RTISolver.html',1,'Optimization']]]
 ];
--- search/classes_f.js
+++ search/classes_f.js
@@ -2,13 +2,14 @@ var searchData=
 [
   ['range_0',['Range',['../structOptimization_1_1Range.html',1,'Optimization']]],
   ['residualfunc_1',['ResidualFunc',['../structOptimization_1_1RTINMPCController_1_1ResidualFunc.html',1,'Optimization::RTINMPCController']]],
-  ['rmsprop_2',['RMSProp',['../classOptimization_1_1RMSProp.html',1,'Optimization']]],
-  ['rmsproptest_3',['RMSPropTest',['../classOptimization_1_1Test_1_1RMSPropTest.html',1,'Optimization::Test']]],
-  ['rosenbrock2d_4',['Rosenbrock2D',['../structOptimization_1_1Tests_1_1Rosenbrock2D.html',1,'Optimization::Tests']]],
-  ['rosenbrockfunc_5',['RosenbrockFunc',['../structOptimization_1_1Test_1_1RosenbrockFunc.html',1,'Optimization::Test']]],
-  ['rosenbrockfunction_6',['RosenbrockFunction',['../structOptimization_1_1Tests_1_1RosenbrockFunction.html',1,'Optimization::Tests']]],
-  ['rosenbrockresiduals_7',['RosenbrockResiduals',['../structRosenbrockResiduals.html',1,'']]],
-  ['rtinmpccontroller_8',['RTINMPCController',['../classOptimization_1_1RTINMPCController.html',1,'Optimization']]],
-  ['rtiobstacle_9',['RTIObstacle',['../structOptimization_1_1RTINMPCController_1_1RTIObstacle.html',1,'Optimization::RTINMPCController']]],
-  ['rtisolver_10',['RTISolver',['../classOptimization_1_1RTISolver.html',1,'Optimization']]]
+  ['riccatisolver_2',['RiccatiSolver',['../structOptimization_1_1solver_1_1RiccatiSolver.html',1,'Optimization::solver']]],
+  ['rmsprop_3',['RMSProp',['../classOptimization_1_1RMSProp.html',1,'Optimization']]],
+  ['rmsproptest_4',['RMSPropTest',['../classOptimization_1_1Test_1_1RMSPropTest.html',1,'Optimization::Test']]],
+  ['rosenbrock2d_5',['Rosenbrock2D',['../structOptimization_1_1Tests_1_1Rosenbrock2D.html',1,'Optimization::Tests']]],
+  ['rosenbrockfunc_6',['RosenbrockFunc',['../structOptimization_1_1Test_1_1RosenbrockFunc.html',1,'Optimization::Test']]],
+  ['rosenbrockfunction_7',['RosenbrockFunction',['../structOptimization_1_1Tests_1_1RosenbrockFunction.html',1,'Optimization::Tests']]],
+  ['rosenbrockresiduals_8',['RosenbrockResiduals',['../structRosenbrockResiduals.html',1,'']]],
+  ['rtinmpccontroller_9',['RTINMPCController',['../classOptimization_1_1RTINMPCController.html',1,'Optimization']]],
+  ['rtiobstacle_10',['RTIObstacle',['../structOptimization_1_1RTINMPCController_1_1RTIObstacle.html',1,'Optimization::RTINMPCController']]],
+  ['rtisolver_11',['RTISolver',['../classOptimization_1_1RTISolver.html',1,'Optimization']]]
 ];
