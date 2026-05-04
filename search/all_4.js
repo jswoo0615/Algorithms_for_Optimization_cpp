60613dfd43dccc9d55c301ba8a2584e86272790f
jswoo0615 <jswoo0615@users.noreply.github.com>
2026-05-04 01:10:10 +0000

    deploy: 4c90f39c36ba55baf8cab1bc5da93115310aa6d9

--- search/all_4.js
+++ search/all_4.js
@@ -1,9 +1,10 @@
 var searchData=
 [
-  ['elapsed_5fns_0',['elapsed_ns',['../structOptimization_1_1OptimizationResultND.html#a19bc0e48b795d90ccd7bab9489fa652d',1,'Optimization::OptimizationResultND::elapsed_ns'],['../structOptimization_1_1OptimizationResult.html#ae8a31cb0134cd0e355a9f989461b1a41',1,'Optimization::OptimizationResult::elapsed_ns']]],
-  ['elliptic2d_1',['Elliptic2D',['../structOptimization_1_1Tests_1_1Elliptic2D.html',1,'Optimization::Tests']]],
-  ['ellipticquadratic2d_2',['EllipticQuadratic2D',['../structOptimization_1_1Tests_1_1EllipticQuadratic2D.html',1,'Optimization::Tests']]],
-  ['eqpsolver_3',['EQPSolver',['../classEQPSolver.html',1,'']]],
-  ['evaluate_5feqp_4',['evaluate_EQP',['../classOptimization_1_1KKTMonitor.html#ae5096ffb2278997c260b97a885de7e94',1,'Optimization::KKTMonitor']]],
-  ['extract_5fblock_5',['extract_block',['../classStaticMatrix.html#a7f6df05797ecc86684b69928e320bde5',1,'StaticMatrix']]]
+  ['ekf_0',['EKF',['../classOptimization_1_1estimator_1_1EKF.html',1,'Optimization::estimator']]],
+  ['elapsed_5fns_1',['elapsed_ns',['../structOptimization_1_1OptimizationResultND.html#a19bc0e48b795d90ccd7bab9489fa652d',1,'Optimization::OptimizationResultND::elapsed_ns'],['../structOptimization_1_1OptimizationResult.html#ae8a31cb0134cd0e355a9f989461b1a41',1,'Optimization::OptimizationResult::elapsed_ns']]],
+  ['elliptic2d_2',['Elliptic2D',['../structOptimization_1_1Tests_1_1Elliptic2D.html',1,'Optimization::Tests']]],
+  ['ellipticquadratic2d_3',['EllipticQuadratic2D',['../structOptimization_1_1Tests_1_1EllipticQuadratic2D.html',1,'Optimization::Tests']]],
+  ['eqpsolver_4',['EQPSolver',['../classEQPSolver.html',1,'']]],
+  ['evaluate_5feqp_5',['evaluate_EQP',['../classOptimization_1_1KKTMonitor.html#ae5096ffb2278997c260b97a885de7e94',1,'Optimization::KKTMonitor']]],
+  ['extract_5fblock_6',['extract_block',['../classStaticMatrix.html#a7f6df05797ecc86684b69928e320bde5',1,'StaticMatrix']]]
 ];
--- search/classes_4.js
+++ search/classes_4.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['elliptic2d_0',['Elliptic2D',['../structOptimization_1_1Tests_1_1Elliptic2D.html',1,'Optimization::Tests']]],
-  ['ellipticquadratic2d_1',['EllipticQuadratic2D',['../structOptimization_1_1Tests_1_1EllipticQuadratic2D.html',1,'Optimization::Tests']]],
-  ['eqpsolver_2',['EQPSolver',['../classEQPSolver.html',1,'']]]
+  ['ekf_0',['EKF',['../classOptimization_1_1estimator_1_1EKF.html',1,'Optimization::estimator']]],
+  ['elliptic2d_1',['Elliptic2D',['../structOptimization_1_1Tests_1_1Elliptic2D.html',1,'Optimization::Tests']]],
+  ['ellipticquadratic2d_2',['EllipticQuadratic2D',['../structOptimization_1_1Tests_1_1EllipticQuadratic2D.html',1,'Optimization::Tests']]],
+  ['eqpsolver_3',['EQPSolver',['../classEQPSolver.html',1,'']]]
 ];
